d4f8e28f26637bfb38e0dfe760a89cc81c5ec632
Rafael Dantas <rafadantasdasilva54@gmail.com>
2023-11-08 17:55:55 -0300

    primeiro commit do sistema escolar com spring boot

---
+++ Rest-with-Spring-boot-and-java-webSchool/src/test/java/br/com/webSchool/RestWithSpringBootAndJavaWebSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaWebSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
